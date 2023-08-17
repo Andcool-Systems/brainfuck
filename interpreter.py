14492ccb4b5e17d055b211b924377dc0572797ce
andcool-2007 <drolll2007@gmail.com>
2023-08-17 16:06:57 +0300

    1.6

--- interpreter.py
+++ interpreter.py
@@ -1,16 +1,83 @@
 import sys
 import json
 import os
-import utils
+
+
+def format(script, filename):
+    tab = "    "
+    tabCounter = 0
+    f = open(filename, 'w')
+
+    for char in script:
+        match char:
+            case "[":
+                tabCounter += 1
+                f.write("\n")
+                f.write(tab * tabCounter)
+
+            case "]":
+                f.write("\n")
+                f.write(tab * tabCounter)
+                tabCounter -= 1
+        f.write(char)
+    f.close()
+
+def formatCode(code):
+    ignore = [" ", "\n", "	", "	"]
+    code1 = "".join([char for char in code if not char in ignore])
+    splitted = code1.split("#")
+
+    mem = None
+    notOdd = False
+    clearCodeList = []
+    for i, element in enumerate(splitted):
+        if element == "": mem = i
+        elif mem == i - 1:
+            mem = None
+            notOdd = i % 2 == 0
+        elif (i % 2 == 0) != notOdd: clearCodeList.append(element)
+    clearCode = "".join(clearCodeList)
+
+    return clearCode
+
+def init(code):
+    opened = []
+    blocksOfCode = {}
+    portals = {}
+    for i, char in enumerate(code):
+        if char == "[":
+            opened.append(i)
+        if char == "]":
+            if not opened:
+                print(f"Cannot find operator '[' for operator ']' at index {i + 1}")
+                sys.exit()
+            blocksOfCode[i] = opened[-1]
+            startIndex = opened.pop()
+            blocksOfCode[startIndex] = i
+        if char == "G": 
+            if code[i + 1] in portals: 
+                print(f"Can't create more than one output from goto\nGoto name '{code[i + 1]}' on index {i + 1}")
+                sys.exit()
+            portals[code[i + 1]] = i + 1
+
+    if opened:
+        print(f"Operator '[' on index {opened[0] + 1} opened, but not closed.")
+        sys.exit()
+
+    return blocksOfCode, portals
+
 
 #------------------Имя файла-------------------------------
 filename = "file.bf"
+formatBF = False
 for i, param in enumerate(sys.argv):
     if i == 1: 
         filename = str(param)
         if not filename.split(".")[-1] in ["b", "bf"]:
             print(f"File '{filename}' is not a brainfuck script")
             sys.exit()
+    if i == 2 and param == "format": formatBF = True
+        
 #------------------Парсим аргументы------------------------
 if not os.path.isfile("params.json"):
     params = {
@@ -41,7 +108,9 @@ except FileNotFoundError:
     print("No such file or directory")
     sys.exit()
 scriptMid = mainFile.read()
-script = utils.formatCode(scriptMid)
+mainFile.close()
+script = formatCode(scriptMid)
+if formatBF: format(script, filename)
 #---------------------------------------------------------
 
 memory = [0] * (1 if params["memoryManagement"] == "AUTO" else int(params["memorySize"]))
@@ -49,7 +118,7 @@ pointer = 0
 globalInterpreter = 0
 
 #---------------------------------------------------------
-blocksOfCode, portals = utils.init(script)
+blocksOfCode, portals = init(script)
 clipboard = 0
 #---------------------------------------------------------
 while globalInterpreter < len(script):
